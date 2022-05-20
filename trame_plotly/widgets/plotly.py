a4521a76c49978b86db205bf6416b5504a05a517
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-05-20 14:39:22 -0600

    docs(api): add missing information

--- trame_plotly/widgets/plotly.py
+++ trame_plotly/widgets/plotly.py
@@ -17,10 +17,55 @@ ENCODER = encode if has_numpy else no_encoding
 
 
 class Figure(AbstractElement):
+    """
+    Create a Plotly figure element. For more details refer to the component
+    `options documentation <https://plotly.com/javascript/configuration-options/>`_
+
+    Properties:
+
+    :param display_mode_bar:
+    :param scroll_zoom:
+    :param editable:
+    :param static_plot:
+    :param to_image_options:
+    :param mode_bar_buttons_to_remove:
+    :param mode_bar_buttons_to_add:
+    :param locale:
+    :param display_logo:
+    :param responsive:
+    :param double_click_delay:
+
+    Events:
+
+    :param after_export:
+    :param after_plot:
+    :param animated:
+    :param animating_frame:
+    :param animation_interrupted:
+    :param auto_size:
+    :param before_export:
+    :param button_clicked:
+    :param click:
+    :param click_annotation:
+    :param deselect:
+    :param double_click:
+    :param framework:
+    :param hover:
+    :param legend_click:
+    :param legend_double_click:
+    :param relayout:
+    :param restyle:
+    :param redraw:
+    :param selected:
+    :param selecting:
+    :param slider_change:
+    :param slider_end:
+    :param slider_start:
+    :param transitioning:
+    :param transition_interrupted:
+    :param unhover:
+    """
     _next_id = 0
-    """
-    Create a Plotly figure element
-    """
 
     def __init__(self, figure=None, **kwargs):
         Figure._next_id += 1
@@ -83,6 +128,11 @@ class Figure(AbstractElement):
         self.update()
 
     def update(self, plotly_fig=None, **kwargs):
+        """
+        Update the Plotly Figure with new content
+
+        :param plotly_fig: Python plotly figure object
+        """
         if plotly_fig:
             self.__figure_data = plotly_fig
 
@@ -93,6 +143,7 @@ class Figure(AbstractElement):
 
     @property
     def key(self):
+        """Return the name of the state variable used internally"""
         return self.__figure_key
 
     @staticmethod
